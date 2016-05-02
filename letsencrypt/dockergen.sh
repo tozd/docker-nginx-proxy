f69bb9210a6a7c4efee5cc02a8746194fd4d8b40
Mitar <mitar.git@tnode.com>
2016-05-01 17:10:44 -0700

    Enable production.

--- letsencrypt/dockergen.sh
+++ letsencrypt/dockergen.sh
@@ -21,8 +21,8 @@ for host in $HOSTS; do
 done
 
 # TODO: Remove "--test-cert" which is currently using for testing.
-/letsencrypt/letsencrypt-auto --no-self-upgrade --noninteractive --agree-tos --email "${LETSENCRYPT_EMAIL}" \
- --config-dir /ssl/letsencrypt certonly --webroot --test-cert --keep-until-expiring --rsa-key-size 4096 \
+/letsencrypt/letsencrypt-auto --no-self-upgrade --noninteractive --quiet --agree-tos --email "${LETSENCRYPT_EMAIL}" \
+ --config-dir /ssl/letsencrypt certonly --webroot --keep-until-expiring --rsa-key-size 4096 \
  --webroot-map "${LIST_JSON}"
 
 for host in $HOSTS; do
