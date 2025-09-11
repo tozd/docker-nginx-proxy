e328eb615fbdc52243447743b5a0194cea597bd5
Mitar <mitar.git@tnode.com>
2025-09-11 14:02:44 +0200

    Revert "Debug."
    This reverts commit c3666a8da617515f470c4faef902fccd5bdd0d45.

--- test.sh
+++ test.sh
@@ -78,7 +78,7 @@ echo "Sleeping"
 sleep 5
 
 echo "Running proxy Docker image"
-docker run -d --name proxy --network testnet --network-alias site.test -p 80:80 -p 443:443 -e LOG_TO_STDOUT=1 -e "LETSENCRYPT_EMAIL=test@example.com" -e "LETSENCRYPT_ARGS=--server https://pebble:14000/dir --no-verify-ssl" -e "REQUESTS_CA_BUNDLE=/letsencrypt/pebble.minica.pem" -v /var/run/docker.sock:/var/run/docker.sock "${CI_REGISTRY_IMAGE}:${TAG}"
+docker run -d --name proxy --network testnet --network-alias site.test -p 80:80 -p 443:443 -e LOG_TO_STDOUT=1 -e "LETSENCRYPT_EMAIL=test@example.com" -e "LETSENCRYPT_ARGS=--server https://pebble:14000/dir" -e "REQUESTS_CA_BUNDLE=/letsencrypt/pebble.minica.pem" -v /var/run/docker.sock:/var/run/docker.sock "${CI_REGISTRY_IMAGE}:${TAG}"
 cleanup_proxy=1
 
 echo "Running app Docker image"
